546d008e7cf8df2dbb6c8dceb70bfb82ae753e3f
rexdavid06 <rhexmilia06@gmail.com>
2026-03-05 21:14:17 +0100

    added rate limiting

--- task_flow_API/settings.py
+++ task_flow_API/settings.py
@@ -55,6 +55,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRate.Throttle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '1000/day',
+        'anon': '100/day',
+    },
 }
 
 SIMPLE_JWT = {
